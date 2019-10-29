a83c74d73ee28b9570cdd1d2d2b8ce07ccbfe716
Sachin Chandwani <sachinchandwani@sachin.local>
2019-10-29 17:00:23 +0530

    upp
    upp upp upp

--- contracts/Patient.sol
+++ contracts/Patient.sol
@@ -5,31 +5,48 @@ contract Patient{
     string name;
     uint age;
     string gender;
-<<<<<<< HEAD
-//    modifier checkLevel(uint _age){
-//        require(
-//            _age>0,
-//            "Invalid age"
-//            );
-//            _;
-//    }
-=======
-   modifier checkLevel(uint _age){
-       require(
-           _age>0,
-           "Invalid age"
-           );
-           _;
-   }
->>>>>>> 5a341c7586fe451b037d464d87002d6e15bc13d3
-    function set(string memory _name, uint _age, string memory _gender) public {
+    string bg;
+    function set(string memory _name, uint _age, string memory _gender,string memory _bg) public {
         patientCount++;
         name = _name;
         age = _age;
         gender = _gender;
+        bg = _bg;
         // patients[msg.sender].name=_name;
     }
-    function get() public view returns(string memory, uint, string memory){
-    return (name, age, gender) ;
+    function get() public view returns(string memory, uint, string memory,string memory){
+    return (name, age, gender,bg) ;
     }
 }
+
+// pragma solidity ^0.5.0;
+
+// contract Patient{
+//     uint public patientCount = 0;
+//     mapping(uint => Info) public info;
+//     struct Info{
+//     uint id;
+//     string name;
+//     uint age;
+//     string gender;
+//     }
+// //    modifier checkLevel(uint _age){
+// //        require(
+// //            _age>0,
+// //            "Invalid age"
+// //            );
+// //            _;
+// //    }
+//     function set(string memory _name, uint _age, string memory _gender) public {
+//         patientCount++;
+//         info[patientCount] = Info(patientCount, _name,_age,_gender);
+//         // id=_id;
+//         // name = _name;
+//         // age = _age;
+//         // gender = _gender;
+//         // patients[msg.sender].name=_name;
+//     }
+//     function get() public view returns(string memory _name, uint _age, string memory _gender){
+//     return (info[patientCount].name, info[patientCount].age, info[patientCount].gender) ;
+//     }
+// }
